673fa5690808c6a88eaa0492bbc78d2413747689
Heather Conklin <heath3conk@gmail.com>
2017-01-29 12:23:55 -0600

    Update isogram challenge with alternate solutions as class and object

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,18 +1,52 @@
-var Isogram = function(givenString) {
-  this.givenString = givenString;
-}
+// version 1: isogram as a class
 
-Isogram.prototype.isIsogram = function(){
-  var stringArray = this.givenString.toLowerCase().split(/\u0020|-|/).sort()
-  for (var i = 0; i < stringArray.length - 1; i++ ) {
-    if (stringArray[i] == stringArray[i+1]) {
-     return false
+// class Isogram {
+
+//   constructor(givenString) {
+//     this.givenString = givenString;
+//   }
+
+//   isIsogram() {
+//     var stringArray = this.givenString.toLowerCase().split(/\u0020|-|/).sort()
+//     for (var i = 0; i < stringArray.length - 1; i++ ) {
+//       if (stringArray[i] == stringArray[i+1]) {
+//        return false
+//       }
+//       else {
+//         var check = true
+//       }
+//     }
+//     return check
+//     }
+// }
+// module.exports = Isogram;
+
+// version 2: isogram as an object
+
+
+var Isogram = function(word){
+  this.word = word,
+
+  this.prepareWord = function (inputWord){
+    var firstClean = inputWord.replace(/[@.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var secondClean = firstClean.replace(/\s+/g,"");
+    return secondClean.toLowerCase().split("");
+  },
+  
+  this.isIsogram = function(){
+    var arrayOfLetters = this.prepareWord(this.word);
+    var letterCount = {};
+    var isogram = true;
+    for(var i = 0; i < arrayOfLetters.length; i++){
+      if(letterCount.hasOwnProperty(arrayOfLetters[i])){
+        letterCount[arrayOfLetters[i]] += 1;
+        isogram = false;
+      } else {
+        letterCount[arrayOfLetters[i]] = 1;
+      }
     }
-    else {
-      var check = true
-    }
+    return isogram;
   }
-  return check
 }
 
 module.exports = Isogram;
